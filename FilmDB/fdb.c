ba8b32d019f8ea2f4b616c1704675f87158fc6c6
Edward Toth <e0725631@student.tuwien.ac.at>
2014-11-13 16:29:02 +0100

    newer fdb version

--- FilmDB/fdb.c
+++ FilmDB/fdb.c
@@ -1,96 +1,133 @@
 #include <dirent.h>
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <limits.h>
 
+/* use strrchr(char *string, int c) to find last occurence of a character c in string string*/
 
-void read_dirs(char *p, void (*fcn) (char *)){
+struct meta_f{
+	char patH[NAME_MAX];
+	char name[NAME_MAX];
+};
 
-	DIR *dp;
-	struct dirent *rid;
-	char d_nam[NAME_MAX];
+void read_files(char *path, char *prev_d, char *file);
 
-	if((dp = opendir(p)) == NULL){
-		fprintf(stderr, "Cannot open directory %s\n", p);
-		return;
-	}
-	while((rid = readdir(dp)) != NULL){
-		if('.'!= rid->d_name[0]){
-			sprintf(d_nam, "%s/%s", p, rid->d_name);
-			(*fcn)(d_nam);	
-			if(strstr(p, "Serien/")){
-				break;
-			}
+
+int main(void){
+
+	FILE *input/*, *infout*/;
+	int i = 0, a = 1, cnt = 0;
+	char **paths, buf[NAME_MAX];
+
+
+	input = fopen("path_to_files.dat", "r");
+		if((paths = (char**)malloc(a*sizeof(char*))) == NULL){
+			return 1;
 		}
+
+	if(input != NULL){
+		fscanf(input, "%d", &a);
+
+		for(i=0;i<a;i++){
+			if((paths[i] = (char*)malloc(NAME_MAX)) == NULL){
+				return 1;
+			}
+			fscanf(input,"%s",&paths[i][0]);
+		}
+
+		fclose(input);
+	} else {
+		/*if((paths = (char**)malloc(sizeof(char*))) == NULL){
+			return 1;
+		}*/
+		if((paths[0] = (char*)malloc(NAME_MAX)) == NULL){
+			return 1;
+		}
+
+		printf("Enter an absolut path\n");
+		scanf("%s", &paths[0][0]);
 	}
-		(void) closedir(dp);
-		return;
+
+	//Remove the temporary file, which stores the film names
+	if(remove("Films.tmp") != 0){
+		fprintf(stderr, "Cannot remove file!");
+	}
+
+	for(i=0;i<a;i++){
+		read_files(paths[i], "", "");
+	}
+
+	input = fopen("Films.tmp", "r");
+
+	while(fscanf(input, "%s", buf) != EOF){
+		cnt++;		
+	}
+	fclose(input);
+
+
+	return 0;
 }
 
-void read_files(char *path){
+
+void read_files(char *path, char *prev_d, char *file){
 
 	struct stat buff;
+	char new_p[NAME_MAX];
 
-	if(stat(path, &buff) == -1){
+	if(strcmp(file,"") != 0){
+		sprintf(new_p, "%s/%s",path, file);
+	} else {
+		strcpy(new_p, path);
+	}
+
+	if(stat(new_p, &buff) == -1){
 		fprintf(stderr, "Cannot access %s\n", path);
 		return;
 	}
 
 	if((buff.st_mode & S_IFMT) == S_IFDIR){
-		read_dirs(path, read_files);	
-	} else if((strstr(path, ".avi") || 
-		strstr(path, ".mov") || 
-		strstr(path, ".mkv") || 
-		strstr(path, ".mpg") || 
-		strstr(path, ".mpeg") ||
-		strstr(path, ".wmv") || 
-		strstr(path, ".mp4") ||
-		strstr(path, ".swf") ||
-		strstr(path, ".flv")) && !strstr(path, ".part")){
-		printf("%s\n", path);
-	}
-	return;
-}
 
-int main(){
+		DIR *dp;
+		struct dirent *rid;
+		char d_nam[NAME_MAX];
 
-	FILE *fpath/*, *infout*/;
-	int i = 0, a = 1;
-	char **paths;
-
-	fpath = fopen("path_to_files.dat", "r");
-
-	if(fpath != NULL){
-		fscanf(fpath, "%d", &a);
-		if((paths = (char**)malloc(a*sizeof(char*))) == NULL){
-			return 1;
+		if((dp = opendir(new_p)) == NULL){
+			fprintf(stderr, "Cannot open directory %s\n", new_p);
+			return;
 		}
-		for(i=0;i<a;i++){
-			if((paths[i] = (char*)malloc(101)) == NULL){
-				return 1;
+		while((rid = readdir(dp)) != NULL){
+			if('.'!= rid->d_name[0]){
+				//sprintf(d_nam, "%s/%s", new_p, rid->d_name);
+				read_files(new_p, file, rid->d_name);	
 			}
-			fscanf(fpath,"%s",&paths[i][0]);
 		}
+		(void) closedir(dp);
 
-		fclose(fpath);
-	} else {
-		if((paths = (char**)malloc(sizeof(char*))) == NULL){
-			return 1;
-		}
-		if((paths[0] = (char*)malloc(101)) == NULL){
-			return 1;
-		}
-		printf("Enter a path\n");
-		scanf("%s", &paths[0][0]);
+	} else if((strstr(new_p, ".avi") || 
+				strstr(new_p, ".mkv") || 
+				strstr(new_p, ".mp4") || 
+				strstr(new_p, ".mpg") || 
+				strstr(new_p, ".wmv") ||
+				strstr(new_p, ".mov") || 
+				strstr(new_p, ".mpeg") ||
+				strstr(new_p, ".swf") ||
+				strstr(new_p, ".flv")) && !(strstr(new_p, ".part") || strstr(new_p, "sample"))){
+/*
+		Remove path and only keep file name
+
+		char *name;
+	       	name =  strrchr(new_p, '/') + 1 ;
+*/
+		FILE *outp;
+		outp = fopen("Films.tmp", "a");
+		fprintf(outp,"%s\n", new_p);
+		printf("%s\n", new_p);
+		close(outp);
 	}
 
+	return;
 
-	for(i=0;i<a;i++){
-		read_files(paths[i]);
-	}
-
-	return 0;
 }
